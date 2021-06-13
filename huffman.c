c39c0033cb61bf49e91ff48b1d3807dea81205b9
Love <love.lindqvist101@gmail.com>
2021-06-13 20:05:57 +0200

    Initial commit

---
+++ huffman.c
@@ -0,0 +1,34 @@
+#include<stdio.h> // define the header file
+
+typedef struct B{
+    union {
+        struct B* leftSubBranch;
+        struct {
+            short leftAmount0;
+            char leftAmount1;
+            char leftChar;
+        };
+    };
+    union {
+        struct B* rightSubBranch;
+        struct {
+            short rightAmount0;
+            char rightAmount1;
+            char rightChar;
+        };
+    };
+} Branch;
+
+
+void compress(char* string)
+{
+
+}
+
+void main()
+{
+    char* string ="Text to be compressed";
+    compress(string);
+}
+
+
