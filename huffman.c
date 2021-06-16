803dcce9ca9fcc0f8ec026bd693ce79780c364e5
Love <love.lindqvist101@gmail.com>
2021-06-16 15:50:40 +0200

    Began work on part 1: Creating the huffman tree

--- huffman.c
+++ huffman.c
@@ -1,23 +1,29 @@
 #include<stdio.h> // define the header file
 #include <stdlib.h>
 
+#define leftHasChar     1<<0
+#define rightHasChar    1<<1
+#define semiNull        1<<2
+#define true            1
+
 typedef struct B{
     union {
         struct B* leftSubBranch;
         struct {
-            short leftAmount0;
-            char leftAmount1;
+            char leftAmount;
             char leftChar;
+            char unusedLeft;
         };
     };
     union {
         struct B* rightSubBranch;
         struct {
-            short rightAmount0;
-            char rightAmount1;
+            short rightAmount;
             char rightChar;
+            char unusedRight;
         };
     };
+    char code;
 } Branch;
 
 typedef struct Bitfield{
@@ -32,9 +38,132 @@ typedef struct Compressor{
     Branch* branch;
 }Compressor;
 
+typedef struct LL{
+    Branch* this;
+    struct LL* next;
+}LinkedList;
+
+void createAndAdd(LinkedList* list, Branch* item){
+    LinkedList* newListItem = (LinkedList*) malloc(sizeof(LinkedList));
+    newListItem->this = item;
+    newListItem->next=0;
+    list->next = newListItem;
+}
+
+void add(LinkedList* list, Branch* item){
+    if (list->next == 0) createAndAdd(list, item);
+    else add(list->next, item);
+}
+
+LinkedList* newLinkedList(){
+    LinkedList* list = (LinkedList*) malloc(sizeof(LinkedList));
+    list->next = 0;
+}
+
+char* countChars(char* string){
+    char* charCount = (char*) malloc(255);
+    for (int i = 0; i < 256; i++) *(charCount+i) = 0;
+    while(*string)
+    {
+        *(charCount+*string++) += 1;
+        charCount[*string++]++;
+    }
+    return charCount;
+}
+
+Branch* newSemiNull(unsigned char c, char amount){
+    Branch* b = (Branch*) malloc(sizeof(Branch));
+    b->leftChar = c;
+    b->leftAmount = amount;
+    b->rightSubBranch = 0;
+    b->code |= semiNull;
+    return b;
+}
+
+int valueOf(Branch item){
+    if (item.code & semiNull) return item.leftAmount;
+    int leftValue;
+    if (item.code & leftHasChar) leftValue = item.leftAmount;
+    else leftValue = valueOf(*item.leftSubBranch);
+    int rightValue;
+    if (item.code & rightHasChar) rightValue = item.rightAmount;
+    else rightValue = valueOf(*item.rightSubBranch);
+    return leftValue + rightValue;
+}
+
+int compare(Branch* b1, Branch* b2){
+    int v1 = valueOf(*b1);
+    int v2 = valueOf(*b2);
+    if (v1<v2) return -1;
+    if (v1>v2) return 1;
+    return 0;
+}
+
+LinkedList* sort(LinkedList* list){
+    LinkedList* first = list;
+    LinkedList* twoBack = 0;
+    LinkedList* oneBack = first;
+    LinkedList* current = oneBack->next;
+    while (true){
+        int diff = compare(oneBack->this, current->this);
+        if (diff <= 0){
+            twoBack=oneBack;
+            oneBack=current;
+            current=current->next;
+            if (current==0) break;
+        } else {
+            if (twoBack == 0){
+                first = current;
+            } else {
+                twoBack->next = current;
+            }
+            oneBack->next = current->next;
+            current->next = oneBack;
+            twoBack=0;
+            oneBack=first;
+            current = oneBack->next;
+        }
+    }
+}
+
+LinkedList* createLinkedList(char* charCount){
+    LinkedList* tmp = newLinkedList();
+    for (int i = 0; i < 256; i++){
+        char c = *(charCount+i);
+        if (c) add(tmp, newSemiNull((unsigned char)i, c));
+    }
+    tmp = tmp->next;        //the first item is a misc
+    tmp = sort(tmp);
+    return tmp;
+}
+
+Branch* createTree(LinkedList* list){
+    LinkedList* thirdLast;
+    LinkedList* secondLast;
+    LinkedList* last;
+    while (list->next != 0){
+        thirdLast = list;
+        secondLast = list->next;
+        if (secondLast->next == 0){
+            last = secondLast;
+            secondLast = list;
+        } else {
+            last = secondLast->next;
+            while (last->next){
+                last = last->next;
+                secondLast = secondLast->next;
+                thirdLast = thirdLast->next;
+            }
+        }
+        insert(list, combinedBranch(secondLast, last));
+    }
+}
+
 Compressor compress(char* string)
 {
-
+    char* charCount = countChars(string);
+    LinkedList* list = createLinkedList(charCount);
+    Branch* tree = createTree(list);
 }
 
 char* unCompress(Compressor compressor){
@@ -43,9 +172,9 @@ char* unCompress(Compressor compressor){
 
 void main()
 {
-    char* string ="Text to be compressed via huffmancompression";
+    char* string ="ABCDEFGHIJKLMNOPabcdefhijklmnop";
     Compressor compressedString = compress(string);
-    printf(unCompress(compressedString));
+    //printf(unCompress(compressedString));
 }
 
 
